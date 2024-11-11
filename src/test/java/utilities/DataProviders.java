7cd6bda7b7bdff2ea50399285d3808d6c2e2caba
Abhishek Goyal <abgoyal@altair.com>
2024-11-11 09:05:37 +0530

    utilities file added

---
+++ src/test/java/utilities/DataProviders.java
@@ -0,0 +1,32 @@
+package utilities;
+import org.testng.annotations.DataProvider;
+import java.io.IOException;
+
+
+public class DataProviders {
+
+	String path=".\\TestData\\SpotifyTestData.xlsx";
+	public String [][] getData(String SheetName) throws IOException
+	{	
+		ExcelUtility xlutil=new ExcelUtility(path);		
+		int totalrows=xlutil.getRowCount(SheetName);	
+		int totalcols=xlutil.getCellCount(SheetName,1);
+		String data[][]=new String[totalrows][totalcols];	
+		for(int i=1;i<=totalrows;i++)
+		{		
+			for(int j=0;j<totalcols;j++)
+			{
+				data[i-1][j]= xlutil.getCellData(SheetName,i, j);
+			}
+		}
+	return data;
+				
+	}
+
+	@DataProvider(name="LoginData")
+	public String [][] getLoginData() throws IOException
+	{	
+		return getData("Login");
+				
+	}
+}
