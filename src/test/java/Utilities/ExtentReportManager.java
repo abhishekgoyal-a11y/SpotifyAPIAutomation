8b09053b561758419a711db904ad2cb0f32897a9
Abhishek Goyal <abgoyal@altair.com>
2024-11-12 14:05:32 +0530

    report file code updated

--- src/test/java/Utilities/ExtentReportManager.java
+++ src/test/java/Utilities/ExtentReportManager.java
@@ -38,46 +38,25 @@ public class ExtentReportManager implements ITestListener {
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		test = extent.createTest(result.getName());
+		test = extent.createTest((String) result.getParameters()[0]);
 		test.log(Status.PASS, "Test Passed");
 	}
 
 	public void onTestFailure(ITestResult result) {
-		test = extent.createTest(result.getName());
+		test = extent.createTest((String) result.getParameters()[0]);
 		test.log(Status.FAIL, "Test Failed");
 		test.log(Status.FAIL, result.getThrowable().getMessage());
 
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		test = extent.createTest(result.getName());
+		test = extent.createTest((String) result.getParameters()[0]);
 		test.log(Status.SKIP, "Test Skipped");
 		test.log(Status.SKIP, result.getThrowable().getMessage());
 	}
 
 	public void onFinish(ITestContext testContext) {
 		extent.flush();
-
-		/*
-		 * try { URL url = new
-		 * URL("file:///"+System.getProperty("user.dir")+"\\reports\\"+repName);
-		 * 
-		 * // Create the email message 
-		 * ImageHtmlEmail email = new ImageHtmlEmail();
-		 * email.setDataSourceResolver(new DataSourceUrlResolver(url));
-		 * email.setHostName("smtp.googlemail.com"); 
-		 * email.setSmtpPort(465);
-		 * email.setAuthenticator(new DefaultAuthenticator("pavanoltraining@gmail.com","password")); 
-		 * email.setSSLOnConnect(true);
-		 * email.setFrom("pavanoltraining@gmail.com"); //Sender
-		 * email.setSubject("Test Results");
-		 * email.setMsg("Please find Attached Report....");
-		 * email.addTo("pavankumar.busyqa@gmail.com"); //Receiver 
-		 * email.attach(url, "extent report", "please check report..."); 
-		 * email.send(); // send the email 
-		 * }
-		 * catch(Exception e) { e.printStackTrace(); }
-		 */
 	}
 
 }
